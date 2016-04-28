d7abfa426b8db002e300e620b4baed77cf638b9a
Tommi Honkala <tommi.honkala@datakala.fi>
2016-04-28 23:34:37 +0300

    Revert "Commit #2"
    This reverts commit 31f55b1f74bff611de042144d30a3e8bf2c30361.

--- js/game.js
+++ js/game.js
@@ -4,35 +4,28 @@
       var i = 0;
 			var sun, mercury, venus, earth, mars, jupiter, saturn, uranus, neptune;
 
-			var particleGeometry, particleCount;
-			var materials = [];
-			var parameterCount, particles;
-
 			function init() {
 				container = document.getElementById( 'container' );
 
-				camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 500000 );
+				camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 150000 );
 				camera.position.set(0,-10000,10000);
 
-        cameraControls = new THREE.TrackballControls(camera);
-        cameraControls.target.set(0, 0, 0);
+        cameraControls = new THREE.TrackballControls(camera, container);
+        //cameraControls.target.set(0, 0, 0);
 
 				scene = new THREE.Scene();
 				//scene.fog = new THREE.Fog( 0x000000, 1, 50000 );
-
+				//
 				scene.add( new THREE.AmbientLight( 0x404040 ) );
-
-				var sunLight = new THREE.PointLight( 0xffffff, 2, 1000000 );
-				sunLight.position.set( 0, 0, 0);
-				scene.add( sunLight );
-
-				var light2 = new THREE.DirectionalLight( 0xffffff, 0.1 );
+				var light1 = new THREE.DirectionalLight( 0xffffff, 8 );
+				light1.position.set( 0, 0, 0);
+				scene.add( light1 );
+				var light2 = new THREE.DirectionalLight( 0xffffff, 1 );
 				light2.position.set( 0,-10000,10000 );
 				scene.add( light2 );
 
 				//Auringon materiaali
-				var materialSun = new THREE.MeshPhongMaterial( {map: THREE.ImageUtils.loadTexture('img/sun.jpg'), opacity: .5, transparent: true, shading: THREE.SmoothShading} );
-				materialSun.depthWrite = false;
+				var materialSun = new THREE.MeshPhongMaterial( {map: THREE.ImageUtils.loadTexture('img/sun.jpg')} );
 
 				//Merkuriuksen materiaali
 				var materialMercury = new THREE.MeshPhongMaterial( {map: THREE.ImageUtils.loadTexture('img/mercury.jpg')} );
@@ -66,20 +59,19 @@
 				//Neptunuksen materiaali
 				var materialNeptune = new THREE.MeshPhongMaterial( {map: THREE.ImageUtils.loadTexture('img/neptune.jpg')} );
 
-				var sizemultiplier = 3;
 				sun = new THREE.Mesh( new THREE.SphereGeometry(300,32,32), materialSun );
-				mercury = new THREE.Mesh( new THREE.SphereGeometry(27*sizemultiplier,32,32), materialMercury );
-				venus = new THREE.Mesh( new THREE.SphereGeometry(67*sizemultiplier,32,32), materialVenus );
-				earth = new THREE.Mesh( new THREE.SphereGeometry(70*sizemultiplier,32,32), materialEarth );
-					moon = new THREE.Mesh( new THREE.SphereGeometry(20*sizemultiplier,32,32), materialMoon );
-				mars = new THREE.Mesh( new THREE.SphereGeometry(37*sizemultiplier,32,32), materialMars );
-				jupiter = new THREE.Mesh( new THREE.SphereGeometry(784*sizemultiplier,32,32), materialJupiter );
-				saturn = new THREE.Mesh( new THREE.SphereGeometry(662*sizemultiplier,32,32), materialSaturn );
-				uranus = new THREE.Mesh( new THREE.SphereGeometry(280*sizemultiplier,32,32), materialUranus );
-				neptune = new THREE.Mesh( new THREE.SphereGeometry(272*sizemultiplier,32,32), materialNeptune );
+				mercury = new THREE.Mesh( new THREE.SphereGeometry(27,32,32), materialMercury );
+				venus = new THREE.Mesh( new THREE.SphereGeometry(67,32,32), materialVenus );
+				earth = new THREE.Mesh( new THREE.SphereGeometry(70,32,32), materialEarth );
+					moon = new THREE.Mesh( new THREE.SphereGeometry(20,32,32), materialMoon );
+				mars = new THREE.Mesh( new THREE.SphereGeometry(37,32,32), materialMars );
+				jupiter = new THREE.Mesh( new THREE.SphereGeometry(784,32,32), materialJupiter );
+				saturn = new THREE.Mesh( new THREE.SphereGeometry(662,32,32), materialSaturn );
+				uranus = new THREE.Mesh( new THREE.SphereGeometry(280,32,32), materialUranus );
+				neptune = new THREE.Mesh( new THREE.SphereGeometry(272,32,32), materialNeptune );
 
 				//Lisätään aurinko, planeetat ja kuut
-				//scene.add( sun );
+				scene.add( sun );
 				scene.add( mercury );
 				scene.add( venus );
 				scene.add( earth );
@@ -90,82 +82,28 @@
 				scene.add( uranus );
 				scene.add( neptune );
 
-				//Lisätään tähdet
-				particleGeometry = new THREE.SphereGeometry(500); /*	NO ONE SAID ANYTHING ABOUT MATH! UGH!	*/
-
-				particleCount = 10000; /* Leagues under the sea */
-
-				/*	Hope you took your motion sickness pills;
-				We're about to get loopy.	*/
-
-				for (i = 0; i < particleCount; i++) {
-
-						var vertex = new THREE.Vector3();
-						vertex.x = Math.random() * 20000 - 10000;
-						vertex.y = Math.random() * 20000 - 10000;
-						vertex.z = Math.random() * 20000 - 10000;
-
-						particleGeometry.vertices.push(vertex);
-				}
-
-				/*	We can't stop here, this is bat country!	*/
-
-				parameters = [
-						[
-								[1, 1, 0.5], 5
-						],
-						[
-								[0.95, 1, 0.5], 4
-						],
-						[
-								[0.90, 1, 0.5], 3
-						],
-						[
-								[0.85, 1, 0.5], 2
-						],
-						[
-								[0.80, 1, 0.5], 1
-						]
-				];
-				parameterCount = parameters.length;
-
-				/*	I told you to take those motion sickness pills.
-				Clean that vommit up, we're going again!	*/
-
-				for (i = 0; i < parameterCount; i++) {
-
-						color = parameters[i][0];
-						size = parameters[i][1];
-
-						materials[i] = new THREE.PointCloudMaterial({
-								size: size
-						});
-
-						particles = new THREE.PointCloud(particleGeometry, materials[i]);
-
-						particles.rotation.x = Math.random() * 6;
-						particles.rotation.y = Math.random() * 6;
-						particles.rotation.z = Math.random() * 6;
-
-						scene.add(particles);
-				}
-
-
 				//Lisätään Saturnuksen renkaat
-				var geometryRingSaturn = new THREE.RingGeometry( 2500, 3500, 32 );
+				var geometryRingSaturn = new THREE.RingGeometry( 690, 740, 32 );
 				ringSaturn = new THREE.Mesh( geometryRingSaturn, materialRingSaturn );
 				scene.add( ringSaturn );
 
-				//Lisätään auringon efektivalo
-				addLight(scene,0.55, 0.9, 0.5,0,0,0);
+				//Lisätään tähtiä tai jotain
+				var geometry = new THREE.SphereGeometry(5, 10, 2);
+				var material = new THREE.MeshDepthMaterial();
 
-				//Lisätään apuruudukko
-				var grid = new THREE.GridHelper(100000, 2000);
-				grid.rotation.x = -1.57;
-				grid.setColors(0xffffff, 0x555555);
-				grid.position.z = -1000;
-				scene.add(grid);
+				group = new THREE.Group();
+				for ( var i = 0; i < 20000; i ++ ) {
+					var mesh = new THREE.Mesh( geometry, material );
+					mesh.position.x = Math.random() * 60000 - 10000;
+					mesh.position.y = Math.random() * 60000 - 10000;
+					mesh.position.z = Math.random() * 60000 - 10000;
+					mesh.matrixAutoUpdate = false;
+					mesh.updateMatrix();
+					group.add( mesh );
+				}
+				scene.add( group );
 
+				//
 				renderer = new THREE.WebGLRenderer( { antialias: true, alpha : true } );
 				renderer.setClearColor(0x000000, 0);
 				renderer.setPixelRatio( window.devicePixelRatio );
@@ -175,88 +113,65 @@
 				//
 				window.addEventListener( 'resize', onWindowResize, false );
 			}
-
-			function addLight(scene, h, s, l, x, y, z ) {
-			        THREE.ImageUtils.crossOrigin = '';
-			        var textureFlare0 = THREE.ImageUtils.loadTexture('img/lensflare0.png');
-
-			    var light = new THREE.PointLight( 0xffffff, 1.5, 10 );
-			    light.color.setHSL( h, s, l );
-			    light.position.set( x, y, z );
-			    scene.add( light );
-			    light = light;
-
-			    var flareColor = new THREE.Color( 0xffffff );
-			    flareColor.setHSL( h, s, l + 0.5 );
-
-			    var lensFlare = new THREE.LensFlare( textureFlare0, 800, 0.0, THREE.AdditiveBlending, flareColor );
-
-			    lensFlare.position.copy( light.position );
-			    var lensFlare = lensFlare;
-
-			    scene.add( lensFlare );
-			}
-
 			function onWindowResize() {
 				camera.aspect = window.innerWidth / window.innerHeight;
 				camera.updateProjectionMatrix();
 				renderer.setSize( window.innerWidth-40, window.innerHeight-40 );
 			}
-
 			function orbit(angle) {
 				var time = Date.now() * 0.001;
-				var earthSpeed = 0.000005;
-				var distanceMultiplier = 3;
+				var earthSpeed = 0.00002;
 
 				sun.rotation.x = 45;
 				sun.rotation.y = time * 0.05;
 
-				mercury.position.x = Math.sin(Math.PI * Date.now()*earthSpeed*4)*774*distanceMultiplier;
-				mercury.position.y = Math.cos(Math.PI * Date.now()*earthSpeed*4)*774*distanceMultiplier;
-				mercury.rotation.x = 360;
+				mercury.position.x = Math.sin(Math.PI * Date.now()*earthSpeed*4)*774;
+				mercury.position.y = Math.cos(Math.PI * Date.now()*earthSpeed*4)*774;
+				mercury.rotation.x = 45;
 				mercury.rotation.y = time * -1;
 
-				venus.position.x = Math.sin(Math.PI * Date.now()*earthSpeed*1.7)*1446*distanceMultiplier;
-				venus.position.y = Math.cos(Math.PI * Date.now()*earthSpeed*1.7)*1446*distanceMultiplier;
-				venus.rotation.x = 360;
+				venus.position.x = Math.sin(Math.PI * Date.now()*earthSpeed*1.7)*1446;
+				venus.position.y = Math.cos(Math.PI * Date.now()*earthSpeed*1.7)*1446;
+				venus.rotation.x = 45;
 				venus.rotation.y = time * -1;
 
-				earth.position.x = Math.sin(Math.PI * Date.now()*earthSpeed)*2000*distanceMultiplier;
-				earth.position.y = Math.cos(Math.PI * Date.now()*earthSpeed)*2000*distanceMultiplier;
-				earth.rotation.x = 360;
+				earth.position.x = Math.sin(Math.PI * Date.now()*earthSpeed)*2000;
+				earth.position.y = Math.cos(Math.PI * Date.now()*earthSpeed)*2000;
+				earth.rotation.x = 45;
 				earth.rotation.y = time * -1;
-							moon.position.x = earth.position.x + Math.sin(Math.PI * Date.now()*earthSpeed*20)*700;
-							moon.position.y = earth.position.y + Math.cos(Math.PI * Date.now()*earthSpeed*20)*700;
-							moon.rotation.x = 360;
+							moon.position.x = earth.position.x + Math.sin(Math.PI * Date.now()*earthSpeed*20)*150;
+							moon.position.y = earth.position.y + Math.cos(Math.PI * Date.now()*earthSpeed*20)*150;
+							moon.rotation.x = 45;
 							moon.rotation.y = time * 0.5;
 
-				mars.position.x = Math.sin(Math.PI * Date.now()*earthSpeed*0.53)*3046*distanceMultiplier;
-				mars.position.y = Math.cos(Math.PI * Date.now()*earthSpeed*0.53)*3046*distanceMultiplier;
-				mars.rotation.x = 360;
+				mars.position.x = Math.sin(Math.PI * Date.now()*earthSpeed*0.53)*3046;
+				mars.position.y = Math.cos(Math.PI * Date.now()*earthSpeed*0.53)*3046;
+				mars.rotation.x = 45;
 				mars.rotation.y = time * -1;
 
-				jupiter.position.x = Math.sin(Math.PI * Date.now()*earthSpeed*0.08)*10406*distanceMultiplier;
-				jupiter.position.y = Math.cos(Math.PI * Date.now()*earthSpeed*0.08)*10406*distanceMultiplier;
-				jupiter.rotation.x = 350;
+				jupiter.position.x = Math.sin(Math.PI * Date.now()*earthSpeed*0.08)*10406;
+				jupiter.position.y = Math.cos(Math.PI * Date.now()*earthSpeed*0.08)*10406;
+				jupiter.rotation.x = 45;
 				jupiter.rotation.y = time * -1;
 
-				saturn.position.x = Math.sin(Math.PI * Date.now()*earthSpeed*0.03)*19078*distanceMultiplier;
-				saturn.position.y = Math.cos(Math.PI * Date.now()*earthSpeed*0.03)*19078*distanceMultiplier;
+				saturn.position.x = Math.sin(Math.PI * Date.now()*earthSpeed*0.03)*19078;
+				saturn.position.y = Math.cos(Math.PI * Date.now()*earthSpeed*0.03)*19078;
 				saturn.position.z = Math.sin(Math.PI * Date.now()*earthSpeed*0.03)*1000;
-				saturn.rotation.x = 400;
+				saturn.rotation.x = 30;
 				saturn.rotation.y = time * -0.5;
 				saturn.rotation.z = 0;
 							ringSaturn.position.x = saturn.position.x;
 							ringSaturn.position.y = saturn.position.y;
 							ringSaturn.position.z = saturn.position.z;
+							ringSaturn.rotation = saturn.rotation;
 
-				uranus.position.x = Math.sin(Math.PI * Date.now()*earthSpeed*0.01)*38370*distanceMultiplier;
-				uranus.position.y = Math.cos(Math.PI * Date.now()*earthSpeed*0.01)*38370*distanceMultiplier;
+				uranus.position.x = Math.sin(Math.PI * Date.now()*earthSpeed*0.01)*38370;
+				uranus.position.y = Math.cos(Math.PI * Date.now()*earthSpeed*0.01)*38370;
 				uranus.rotation.x = 45;
 				uranus.rotation.y = time * -1;
 
-				neptune.position.x = Math.sin(Math.PI * Date.now()*earthSpeed*0.006)*60122*distanceMultiplier;
-				neptune.position.y = Math.cos(Math.PI * Date.now()*earthSpeed*0.006)*60122*distanceMultiplier;
+				neptune.position.x = Math.sin(Math.PI * Date.now()*earthSpeed*0.006)*60122;
+				neptune.position.y = Math.cos(Math.PI * Date.now()*earthSpeed*0.006)*60122;
 				neptune.rotation.x = 45;
 				neptune.rotation.y = time * -1;
 
@@ -264,27 +179,15 @@
 			//
 			function animate() {
 				requestAnimationFrame( animate );
-				//cameraControls.target.set(earth.position.x, earth.position.y, earth.position.z);
         cameraControls.update();
 				render();
 			}
 			function render() {
 				orbit();
-
-				var time = Date.now() * 0.00005;
-				for (i = 0; i < materials.length; i++) {
-
-            color = parameters[i][0];
-
-            h = (360 * (color[0] + time) % 360) / 360;
-            materials[i].color.setHSL(h, color[1], color[2]);
-        }
-
-
 				renderer.render( scene, camera );
 
-				$('#debug2').text("Kuun kierros: " + moon.rotation.y.toFixed(2));
-				$('#debug3').text("Kuun sijainti: X = " + moon.position.x + ", Y = " + moon.position.y + ", Z = " + moon.position.z);
+				$('#debug2').text("Auringon kierros: " + sun.rotation.y.toFixed(2));
+				$('#debug3').text("Auringon sijainti: X = " + sun.position.x + ", Y = " + sun.position.y + ", Z = " + sun.position.z);
 				$('#debug4').text("Kameran sijainti: X = " + camera.position.x.toFixed(0) + ", Y = " + camera.position.y.toFixed(0) + ", Z = " + camera.position.z.toFixed(0));
 				$('#debug5').text("Kameran suunta: " + camera.rotation.x + ", " + camera.rotation.y + ", " + camera.rotation.z);
 				$('#debug6').text("");
